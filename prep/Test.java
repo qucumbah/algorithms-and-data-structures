3bed77449440a4f81d825b64c9c89da7a2147c68
qucumbah <39967396+qucumbah@users.noreply.github.com>
2021-09-10 07:06:43 +0300

    add assertEquals method to the Test class

--- prep/Test.java
+++ prep/Test.java
@@ -17,4 +17,11 @@ public abstract class Test {
       System.err.println("Assertion failed: " + message);
     }
   }
+
+  protected <T extends Comparable<T>> void assertEquals(T a, T b, String message) {
+    if (a.compareTo(b) != 0) {
+      System.err.println("Assertion failed: " + message);
+      System.err.println("Expected: " + b + "; got: " + a);
+    }
+  }
 }
