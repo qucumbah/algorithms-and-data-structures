259f3bb4ffbc67bdff317ecc7848b408b1d41f8d
qucumbah <39967396+qucumbah@users.noreply.github.com>
2021-09-14 08:52:38 +0300

    small fix to the test framework

--- prep/Test.java
+++ prep/Test.java
@@ -20,6 +20,7 @@ public abstract class Test {
 
   protected <T extends Comparable<T>> void assertEquals(T a, T b, String message) {
     if (a.compareTo(b) != 0) {
+      assertionsFailed = true;
       System.err.println("Assertion failed: " + message);
       System.err.println("Expected: " + b + "; got: " + a);
     }
